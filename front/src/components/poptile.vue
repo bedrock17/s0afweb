f1276283f2ce58feace5e33a1df43f33da763fed
SHK <bedrook@gmail.com>
2020-02-11 20:53:35 +0900

    빌드 결과물 동작안됨 확인중...

---
+++ front/src/components/poptile.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="poptile">
+    <!-- <h1>POPTILE</h1> -->
+    <h2>SCORE : {{ game.score }}</h2>
+
+		<div v-if="game.gameOver"> 
+			<h1> GAME OVER  </h1>
+
+			<h1 class="retry" v-on:click="retryGame"> RETRY? </h1>
+		</div>
+
+		<div v-show="!game.gameOver">
+    <canvas id="cvs" width="245px" height="460px"> </canvas>
+		</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator"
+import { Game } from "../poptile"
+
+@Component
+export default class Poptile extends Vue {
+
+  game: Game = new Game()
+
+  mounted() {
+		console.log("mount!!!!")
+		alert("????")
+    this.game.startGame()
+  }
+
+	retryGame() {
+		this.game.startGame()
+
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+* {
+	-webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+canvas {
+  border: 1px solid black;
+  /* margin-left: 10px; */
+  /* margin-top: 10px; */
+  /* width: 100%; */
+  /* height: 500px; */
+}
+
+.retry {
+	color: #fa0;
+	&:hover {
+		color: #0af;
+	}
+}
+
+.title {
+  color: #00aaff;
+  font-weight: bold;
+  font-size: 20pt;
+  padding-left: 50px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ front/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+		<h1> ???????? </h1>
+    <Poptile/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Poptile from '@/components/poptile.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Poptile
+  }
+}
+</script>
