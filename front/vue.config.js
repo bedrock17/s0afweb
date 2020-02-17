2f32a9f509b42d50cf6c71ca2e9658ef7e5115da
SHK <bedrook@gmail.com>
2020-02-17 23:57:09 +0900

    rebuild

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,18 +1,24 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+	publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
 	outputDir: "../static",
-	configureWebpack: { 
-		optimization: {
-			minimizer: [new UglifyJsPlugin()],
-		},
+	configureWebpack: {
+		plugins: [
+			new UglifyJsPlugin({
+				uglifyOptions: {
+					compress: true,
+					warnings: false,
+					mangle: true
+				}
+			}),
+		]
 	},
-  devServer: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080/" // api 개발서버
-      }
-    }
-  }
+	devServer: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:8080/" // api 개발서버
+			}
+		}
+	}
 };
