f00d7ae98989f727bc6ec599297648f3c806fe95
Changhui Lee <iam@xo.dev>
2022-02-27 00:10:36 +0900

    eslint, solid-app-router 추가

---
+++ front/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const IndexPage: Component = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
