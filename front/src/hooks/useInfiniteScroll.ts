e815c64b5dc54a71815e510a031b0544e580e0bc
Changhui Lee <iam@xo.dev>
2022-03-06 23:24:25 +0900

    feat: add infinite scroll

---
+++ front/src/hooks/useInfiniteScroll.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import type { MutableRefObject } from 'react';
+
+import { useIntersectionObserver } from '~/hooks/useIntersectionObserver';
+
+export const useInfiniteScroll = (
+  ref: MutableRefObject<Element | null>,
+  callback: () => void
+) => {
+  const [lastLoadTime, setLastLoadTime] = useState(0);
+  const shouldLoadMore = useIntersectionObserver(ref, { threshold: 0 });
+
+  useEffect(() => {
+    const currentTime = Date.now();
+    if (shouldLoadMore && currentTime - lastLoadTime >= 100) {
+      setLastLoadTime(currentTime);
+      callback();
+    }
+  }, [shouldLoadMore, callback]);
+};
--- front/src/pages/SingleLeaderboard/index.tsx
+++ front/src/pages/SingleLeaderboard/index.tsx
@@ -1,7 +1,11 @@
 import type Stitches from '@stitches/react';
-import React, { useEffect, useState } from 'react';
+import React, {
+  useCallback,
+  useEffect, useMemo, useRef, useState,
+} from 'react';
 
 import { Leaderboard, LeaderboardItem } from '~/api';
+import { useInfiniteScroll } from '~/hooks/useInfiniteScroll';
 
 import {
   Table, TableRow,
@@ -32,6 +36,18 @@ const getColor = (score: number) => {
 
 const SingleLeaderboardPage = () => {
   const [leaderboard, setLeaderboard] = useState<LeaderboardItem[]>([]);
+  const [page, setPage] = useState(1);
+  const infiniteScrollRef = useRef(null);
+  const itemPerPage = 50;
+  const totalPages = useMemo(() => Math.ceil(leaderboard.length / itemPerPage), [leaderboard]);
+
+  useInfiniteScroll(infiniteScrollRef, useCallback(() => {
+    if (page < totalPages) {
+      setPage(page + 1);
+    }
+  }, [page, setPage, totalPages]));
+
+  console.log(page);
 
   useEffect(() => {
     Leaderboard.get()
@@ -46,25 +62,28 @@ const SingleLeaderboardPage = () => {
     <Wrapper>
       <Title>Single Play Leaderboard</Title>
       <Table>
-        <TableRow>
-          <Th>Name</Th>
-          <Th>Score</Th>
-          <Th>Touches</Th>
-          <Th>Score per touch</Th>
-        </TableRow>
-        <>
-          {
-            leaderboard.map((item, index) => (
-              <TableRow key={item.username}>
-                <Td color={(index === 0 ? 'hyper' : getColor(item.score))} name>{item.username}</Td>
-                <Td>{formatNumber(item.score)}</Td>
-                <Td>{formatNumber(item.touches)}</Td>
-                <Td>{formatNumber(parseFloat((item.score / item.touches).toFixed(1)))}</Td>
-              </TableRow>
-            ))
-          }
-        </>
+        <tbody>
+          <TableRow>
+            <Th>Name</Th>
+            <Th>Score</Th>
+            <Th>Touches</Th>
+            <Th>Score per touch</Th>
+          </TableRow>
+          <>
+            {
+              leaderboard.slice(0, itemPerPage * page).map((item, index) => (
+                <TableRow key={item.username}>
+                  <Td color={(index === 0 ? 'hyper' : getColor(item.score))} name>{item.username}</Td>
+                  <Td>{formatNumber(item.score)}</Td>
+                  <Td>{formatNumber(item.touches)}</Td>
+                  <Td>{formatNumber(parseFloat((item.score / item.touches).toFixed(1)))}</Td>
+                </TableRow>
+              ))
+            }
+          </>
+        </tbody>
       </Table>
+      <div className={'infinite-scroll'} ref={infiniteScrollRef}/>
     </Wrapper>
   );
 };
