5c8da877e575db0495278fcc024164a19b91dcea
Changhui Lee <iam@xo.dev>
2022-03-19 21:07:21 +0900

    feat: navigate to index page when user visit invalid page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import {
+  Routes, Route, Navigate 
+} from 'react-router-dom';
 
 import OnlinePlay from '~/pages/OnlinePlay';
 import OnlinePlayRoom from '~/pages/OnlinePlayRoom';
@@ -12,12 +14,13 @@ import SinglePlayResultPage from './pages/SinglePlayResult';
 function App() {
   return (
     <Routes>
+      <Route index element={<IndexPage />} />
+      <Route path={'/single'} element={<SinglePlayPage />} />
       <Route path={'/single/leaderboard'} element={<SingleLeaderboardPage />} />
       <Route path={'/single/result'} element={<SinglePlayResultPage />} />
-      <Route path={'/single/*'} element={<SinglePlayPage />} />
-      <Route path={'/online/*'} element={<OnlinePlay />} />
+      <Route path={'/online'} element={<OnlinePlay />} />
       <Route path={'/online/room/:id'} element={<OnlinePlayRoom />} />
-      <Route path={'*'} element={<IndexPage />} />
+      <Route path={'*'} element={<Navigate to='/' replace />} />
     </Routes>
   );
 }
